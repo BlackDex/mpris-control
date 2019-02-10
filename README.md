830e0a68d308551c0880cf61be1581e0f9ca282a
BlackDex <black.dex@gmail.com>
2019-02-10 16:36:17 +0100

    Initial commit of this application
    - Created the application code
    - Updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # mpris-control
-Small application to control mpris-2 mediaplayers like spotify, vlc, rhythmbox etc..
+A small application to control mpris-2 mediaplayers like spotify, vlc, rhythmbox etc..<br>
+<br>
+I Created this because the spotify-app (snap) didn't seem to work with the default media key's configuration of Ubuntu 18.04.<br>
+<br>
+This application works with all mpris-2 dbus controlled applications as far as i know.<br>
+If you encounter any issues please report them in the issues section.<br>
+<br>
+**Simple usage:**
+```bash
+# Toggle play/pause
+mpris-control toggle
+
+# Play
+mpris-control play
+
+# Pause
+mpris-control pause
+
+# Next track
+mpris-control next
+
+# Previous track
+mpris-control previous
+```
---
+++ src/main.rs
@@ -0,0 +1,50 @@
+//src main.rs
+use std::env;
+
+extern crate mpris;
+
+use mpris::PlayerFinder;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() == 2 {
+        let action = &args[1];
+        action.trim().to_ascii_lowercase();
+
+        if ! action.is_empty() {
+            match run_action(action) {
+                Ok(msg) => {
+                    println!("{:?}", msg);
+                },
+                Err(err) => {
+                    println!("{:?}", err);
+                }
+            }        
+        }
+    } else {
+        println!("No action has been given!\nPlease use, toggle, play, pause, next or previous");
+    }
+}
+
+fn run_action(action: &str) -> Result<String, String> {
+    let player_finder = PlayerFinder::new()
+        .map_err(|e| format!("Could not connect to D-Bus: {}", e))?;
+
+    let player = player_finder.find_active()
+        .map_err(|e| format!("Could not find any player: {}", e))?;
+
+    if action == "toggle" {
+       player.checked_play_pause().map_err(|e| format!("Could not control player: {}", e))?;
+    } else if action == "play" {
+        player.checked_play().map_err(|e| format!("Could not control player: {}", e))?;
+    } else if action == "pause" {
+        player.checked_pause().map_err(|e| format!("Could not control player: {}", e))?;
+    } else if action == "next" {
+        player.checked_next().map_err(|e| format!("Could not control player: {}", e))?;
+    } else if action == "previous" {
+        player.checked_previous().map_err(|e| format!("Could not control player: {}", e))?;
+    }
+
+    Ok(format!("Action {} run", action))
+}
