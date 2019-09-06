73eed0ded7b69c2bc706b3f9a434db3a3659ff60
Evgeny Badin <treeshateorcs@yahoo.com>
2019-09-06 16:32:55 +0300

    complete rewrite

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,63 @@
 //src main.rs
-use std::env;
-
-extern crate mpris;
-
+use clap::{App, AppSettings, Arg, SubCommand};
 use mpris::PlayerFinder;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let mut ignored_player = "";
-    if args.len() == 2 {
-    } else if args.len() == 3 {
-        ignored_player = if args.len() == 3 { &args[2] } else { "" };
-    } else {
-        eprintln!(
-            "Usage: mpris-control {{toggle|play|pause|next|previous|stop}} [player_to_ignore]"
-        );
-        std::process::exit(1);
-    }
-    let action = &args[1];
-    action.trim().to_ascii_lowercase();
-
-    if !action.is_empty() {
-        match run_action(action, ignored_player) {
-            Ok(msg) => {
-                println!("{:?}", msg);
-            }
-            Err(err) => {
-                println!("{:?}", err);
-            }
+    let matches = App::new("mpris-control")
+        .version("0.1.0")
+        .author("BlackDex")
+        .about("Control MPRIS enabled media players")
+        .setting(AppSettings::SubcommandRequiredElseHelp)
+        .setting(AppSettings::InferSubcommands)
+        .subcommand(SubCommand::with_name("play").about("Play"))
+        .subcommand(SubCommand::with_name("pause").about("Pause"))
+        .subcommand(SubCommand::with_name("next").about("Next song"))
+        .subcommand(SubCommand::with_name("previous").about("Previous song"))
+        .subcommand(SubCommand::with_name("stop").about("Stop playback"))
+        .subcommand(SubCommand::with_name("toggle").about("Toggle playback"))
+        .arg(
+            Arg::with_name("ignore")
+                .short("i")
+                .long("ignore")
+                .value_name("IGNORE")
+                .help("List of players to ignore separated by commas")
+                .takes_value(true)
+                .multiple(false)
+                .conflicts_with("target")
+                .required(false),
+        )
+        .arg(
+            Arg::with_name("target")
+                .short("t")
+                .long("target")
+                .value_name("TARGET")
+                .help("Target player (may only be one)")
+                .takes_value(true)
+                .multiple(false)
+                .conflicts_with("ignore")
+                .required(false),
+        )
+        .get_matches();
+    let ignore = match matches.value_of("ignore") {
+        Some(i) => i,
+        None => "",
+    };
+    let target = match matches.value_of("target") {
+        Some(i) => i,
+        None => "",
+    };
+    let action = matches.subcommand().0;
+    match run_action(action, ignore, target) {
+        Ok(msg) => {
+            println!("{:?}", msg);
+        }
+        Err(err) => {
+            println!("{:?}", err);
         }
     }
 }
 
-fn run_action(action: &str, ignored_player: &str) -> Result<String, String> {
+fn run_action(action: &str, ignore: &str, target: &str) -> Result<String, String> {
     let player_finder =
         PlayerFinder::new().map_err(|e| format!("Could not connect to D-Bus: {}", e))?;
 
@@ -40,32 +65,37 @@ fn run_action(action: &str, ignored_player: &str) -> Result<String, String> {
         .find_all()
         .map_err(|e| format!("Could not find any player: {}", e))?;
 
-    players.retain(|x| x.identity() != ignored_player);
+    for i in ignore.split(',') {
+        players.retain(|x| x.identity() != i);
+    }
+    if target != "" {
+        players.retain(|x| x.identity() == target);
+    }
 
     if players.len() > 0 {
         if action == "toggle" {
             players[0]
-                .checked_play_pause()
+                .play_pause()
                 .map_err(|e| format!("Could not control player: {}", e))?;
         } else if action == "play" {
             players[0]
-                .checked_play()
+                .play()
                 .map_err(|e| format!("Could not control player: {}", e))?;
         } else if action == "pause" {
             players[0]
-                .checked_pause()
+                .pause()
                 .map_err(|e| format!("Could not control player: {}", e))?;
         } else if action == "next" {
             players[0]
-                .checked_next()
+                .next()
                 .map_err(|e| format!("Could not control player: {}", e))?;
         } else if action == "previous" {
             players[0]
-                .checked_previous()
+                .previous()
                 .map_err(|e| format!("Could not control player: {}", e))?;
         } else if action == "stop" {
             players[0]
-                .checked_stop()
+                .stop()
                 .map_err(|e| format!("Could not control player: {}", e))?;
         } else {
             return Err(format!("Unrecognized option: {}", action));
