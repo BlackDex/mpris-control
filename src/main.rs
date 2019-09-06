b97b27509a9af119d9adce5105ea08f27026409b
Evgeny Badin <treeshateorcs@yahoo.com>
2019-09-06 11:52:42 +0300

    ignore a player

--- src/main.rs
+++ src/main.rs
@@ -7,45 +7,65 @@ use mpris::PlayerFinder;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-
+    let mut ignored_player = "";
     if args.len() == 2 {
-        let action = &args[1];
-        action.trim().to_ascii_lowercase();
-
-        if ! action.is_empty() {
-            match run_action(action) {
-                Ok(msg) => {
-                    println!("{:?}", msg);
-                },
-                Err(err) => {
-                    println!("{:?}", err);
-                }
-            }        
-        }
+    } else if args.len() == 3 {
+        ignored_player = if args.len() == 3 { &args[2] } else { "" };
     } else {
-        println!("No action has been given!\nPlease use, toggle, play, pause, stop, next or previous");
+        eprintln!(
+            "No action has been given!\nPlease use, toggle, play, pause, stop, next or previous"
+        );
+        std::process::exit(1);
+    }
+    let action = &args[1];
+    action.trim().to_ascii_lowercase();
+
+    if !action.is_empty() {
+        match run_action(action, ignored_player) {
+            Ok(msg) => {
+                println!("{:?}", msg);
+            }
+            Err(err) => {
+                println!("{:?}", err);
+            }
+        }
     }
 }
 
-fn run_action(action: &str) -> Result<String, String> {
-    let player_finder = PlayerFinder::new()
-        .map_err(|e| format!("Could not connect to D-Bus: {}", e))?;
+fn run_action(action: &str, ignored_player: &str) -> Result<String, String> {
+    let player_finder =
+        PlayerFinder::new().map_err(|e| format!("Could not connect to D-Bus: {}", e))?;
 
-    let player = player_finder.find_active()
+    let mut players = player_finder
+        .find_all()
         .map_err(|e| format!("Could not find any player: {}", e))?;
 
+    players.retain(|x| x.identity() != ignored_player);
+
     if action == "toggle" {
-       player.checked_play_pause().map_err(|e| format!("Could not control player: {}", e))?;
+        players[0]
+            .checked_play_pause()
+            .map_err(|e| format!("Could not control player: {}", e))?;
     } else if action == "play" {
-        player.checked_play().map_err(|e| format!("Could not control player: {}", e))?;
+        players[0]
+            .checked_play()
+            .map_err(|e| format!("Could not control player: {}", e))?;
     } else if action == "pause" {
-        player.checked_pause().map_err(|e| format!("Could not control player: {}", e))?;
+        players[0]
+            .checked_pause()
+            .map_err(|e| format!("Could not control player: {}", e))?;
     } else if action == "next" {
-        player.checked_next().map_err(|e| format!("Could not control player: {}", e))?;
+        players[0]
+            .checked_next()
+            .map_err(|e| format!("Could not control player: {}", e))?;
     } else if action == "previous" {
-        player.checked_previous().map_err(|e| format!("Could not control player: {}", e))?;
+        players[0]
+            .checked_previous()
+            .map_err(|e| format!("Could not control player: {}", e))?;
     } else if action == "stop" {
-        player.checked_stop().map_err(|e| format!("Could not control player: {}", e))?;
+        players[0]
+            .checked_stop()
+            .map_err(|e| format!("Could not control player: {}", e))?;
     } else {
         return Err(format!("Unrecognized option: {}", action));
     }
